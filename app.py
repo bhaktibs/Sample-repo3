9d4269b7bfce1484bdc5c61b5ff0c10f91bbb044
bhaktibs <bhakti.shah@sjsu.edu>
2017-05-10 12:32:33 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import request
+from flask import json
+
+
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "changed from  Flask App!!"
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=5000)
